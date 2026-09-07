b4d051724825bb602ce54e291f996f14b6571888
Renato Caushi <ren.caushi22@gmail.com>
2026-09-07 15:00:08 +0200

    Register media folder inside STATICFILES_DIRS for WhiteNoise

--- portal_lajmesh/settings.py
+++ portal_lajmesh/settings.py
@@ -108,20 +108,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static & Media Files (CSS, JS, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Shto këtë rresht që WhiteNoise të përfshijë dosjen media në përmbajtjen statike
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Email
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
