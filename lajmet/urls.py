fe8ac9e7ae8a68e87dfa97f833f5df39c7612208
Renato Caushi <ren.caushi22@gmail.com>
2026-09-07 23:40:39 +0200

    Fix URL route name for faqja_kryesore

---
+++ lajmet/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.faqja_kryesore, name='faqja_kryesore'),
+    path('lajmi/<int:pk>/', views.detajet_e_lajmit, name='detajet_e_lajmit'),
+]
