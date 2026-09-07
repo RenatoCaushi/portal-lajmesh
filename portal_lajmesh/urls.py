4cf9d2f57a437a11e6f2a0b2093f4aab11451d4d
Renato Caushi <ren.caushi22@gmail.com>
2026-09-07 15:28:11 +0200

    Force-serve media folder and include all images

--- portal_lajmesh/urls.py
+++ portal_lajmesh/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -22,5 +23,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lajmet.urls')),
+    
+    # Kap çdo kërkesë /media/... dhe e shërben nga dosja media
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
