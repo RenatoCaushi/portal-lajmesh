b6a355443ac62aed404906356b9b350ace155698
Renato Caushi <ren.caushi22@gmail.com>
2026-09-09 23:52:04 +0200

    Fix settings and context processor

--- lajmet/context_processors.py
+++ lajmet/context_processors.py
@@ -1,6 +1,11 @@
-from .models import Kategoria
+from .models import Kategoria  # ose emri i modelit tënd për kategoritë
 
 def kategorite_processor(request):
+    try:
+        kategorite = Kategoria.objects.all()
+    except Exception:
+        kategorite = []
+    
     return {
-        'kategorite': Kategoria.objects.all()
+        'kategorite': kategorite
     }
