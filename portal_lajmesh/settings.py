68d7a5ea0a956a8c11888f35a9b74e14230be970
Renato Caushi <ren.caushi22@gmail.com>
2026-09-10 00:03:52 +0200

    Remove inline comment from STORAGES setting

--- portal_lajmesh/settings.py
+++ portal_lajmesh/settings.py
@@ -166,6 +166,6 @@ else:
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",  # Ndryshuar këtu (pa Manifest)
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
         },
     }
