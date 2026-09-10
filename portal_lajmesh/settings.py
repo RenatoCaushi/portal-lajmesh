49274e367c3a47deac34b2d6dbfd4defaf82b83a
Renato Caushi <ren.caushi22@gmail.com>
2026-09-10 23:55:21 +0200

    Remove cloudinary_storage to unblock whitenoise build

--- portal_lajmesh/settings.py
+++ portal_lajmesh/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',  # Duhet të jetë PARA staticfiles
     'django.contrib.staticfiles',
     'cloudinary',
     'lajmet',
