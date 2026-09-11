53675efb30e9a0e325a3d1b2593acb8809c80017
Renato Caushi <ren.caushi22@gmail.com>
2026-09-11 22:23:30 +0200

    Fix detajet_e_lajmit view and youtube script

--- lajmet/views.py
+++ lajmet/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q, F
+from django.db.models import Q
 from django.core.paginator import Paginator
-from .models import Artikull, Kategoria, Koment, Video, Reklama
+from .models import Artikull, Kategoria, Koment, Reklama
 
 
 def faqja_kryesore(request):
+    # Rregullon artikujt pa slug nese ka
     try:
         for art in Artikull.objects.filter(Q(slug__isnull=True) | Q(slug='')):
             art.save()
@@ -25,9 +26,6 @@ def faqja_kryesore(request):
 
     slider_lajmet = lajmet_list[:3] if not kategoria_id else []
     kategorite = Kategoria.objects.all()
-    
-    # Merr 3 videot e fundit nga baza e të dhënave
-    videot_db = list(Video.objects.all().order_by('-id')[:3])
 
     try:
         reklama = Reklama.objects.filter(is_active=True).last()
@@ -44,42 +42,22 @@ def faqja_kryesore(request):
         'kategorite': kategorite,
         'reklama': reklama,
         'kategoria_zgjedhur': kategoria_id,
-        'yt_video_1': videot_db[0].youtube_id if len(videot_db) > 0 else None,
-        'yt_video_2': videot_db[1].youtube_id if len(videot_db) > 1 else None,
-        'yt_video_3': videot_db[2].youtube_id if len(videot_db) > 2 else None,
     }
     return render(request, 'lajmet/index.html', context)
 
 
 def detajet_e_lajmit(request, slug):
     artikull = get_object_or_404(Artikull, slug=slug)
-    
-    try:
-        Artikull.objects.filter(pk=artikull.pk).update(shikime=F('shikime') + 1)
-        artikull.refresh_from_db()
-    except Exception:
-        pass
-
-    if request.method == 'POST':
-        permbajtja = request.POST.get('permbajtja')
-        if permbajtja:
-            Koment.objects.create(
-                artikulli=artikull,
-                emri="Anonim",
-                permbajtja=permbajtja,
-                is_approved=True
-            )
-            return redirect('detajet_e_lajmit', slug=artikull.slug)
-
-    komentet = artikull.komentet.filter(is_approved=True).order_by('-data_publikimit')
+    kategorite = Kategoria.objects.all()
     
     try:
         reklama = Reklama.objects.filter(is_active=True).last()
     except Exception:
         reklama = None
-    
-    return render(request, 'lajmet/detajet.html', {
+
+    context = {
         'artikull': artikull,
-        'komentet': komentet,
+        'kategorite': kategorite,
         'reklama': reklama,
-    })
+    }
+    return render(request, 'lajmet/detajet.html', context)
