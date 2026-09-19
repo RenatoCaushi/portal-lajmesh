6e6bb8ffb9670b1d5455fee32a24acba56c7fd8e
Renato Caushi <ren.caushi22@gmail.com>
2026-09-19 22:39:09 +0200

    Shtuar testimet automatike dhe konfigurimi i SQLite per testet

--- lajmet/tests.py
+++ lajmet/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class PortalLajmeshTests(TestCase):
+    
+    def setUp(self):
+        """Konfigurimi fillestar për çdo test"""
+        self.client = Client()
+
+    def test_homepage_loads_successfully(self):
+        """Teston nëse faqja kryesore e portalit ngarkohet dhe kthen statusin 200 OK"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_albanian_characters_support(self):
+        """Teston mbështetjen për karakteret e veçanta të gjuhës shqipe (ç, ë)"""
+        titulli_testi = "Çështjet e ditës dhe zhvillimet e fundit në vend"
+        
+        # Verifikojmë që stringu me shkronja shqipe përpunohet saktë pa gabime enkodimi
+        self.assertIn("Ç", titulli_testi)
+        self.assertIn("ë", titulli_testi)
+        
+    def test_search_or_invalid_page_handling(self):
+        """Teston se si reagon serveri ndaj një faqeje që nuk ekziston (duhet të kthejë 404)"""
+        response = self.client.get('/faqe-që-nuk-ekziston-koleg/')
+        self.assertEqual(response.status_code, 404)
